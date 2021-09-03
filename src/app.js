e51fa012cebd3b4ae320f1a08cf9513c3ddb6a34
nikkipu <nikkipuyat@gmail.com>
2021-09-03 17:32:02 -0400

    Fixed celsius and farenheit

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ function getInfo (response) {
   let timeElement = document.querySelector('#date-time');
   let iconElement = document.querySelector("#icon");
 
+  farenheitTemperature = response.data.main.temp;
+
   h1Element.innerHTML = response.data.name;
   currentTempElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -49,85 +51,32 @@ function handleSubmit (event) {
   search(searchInput.value);
 }
 
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  farenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemperature = Math.round((farenheitTemperature -32) * 5 / 9);
+  let temperatureElement = document.querySelector(".number");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
+
+function showFarenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  farenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector(".number");
+  temperatureElement.innerHTML = Math.round(farenheitTemperature);
+}
+
+let farenheitTemperature = null;
+
 let form = document.querySelector("#search-bar");
 form.addEventListener("submit", handleSubmit);
 
-// function updateCity(event) {
-//   event.preventDefault();
+let celsiusLink = document.querySelector(".celsius");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
 
-//   let typeCity = document.querySelector("#type-city");
-//   let h1 = document.querySelector("#city");
+let farenheitLink = document.querySelector(".farenheit");
+farenheitLink.addEventListener("click", showFarenheitTemperature);
 
-//   h1.innerHTML = `${typeCity.value}`;
-
-//   let apiKey = "0e058f62fe2dafe733ff53545a2cf37c";
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${typeCity.value}&appid=${apiKey}&units=metric`;
-
-//   axios.get(apiUrl).then(getTemp);
-// }
-
-// function getTemp(response) {
-//   let currentCityTemp = Math.round(response.data.main.temp);
-
-//   let currentTemperature = document.querySelector(".number");
-//   currentTemperature.innerHTML = `${currentCityTemp}°`;
-// }
-
-// let searchCity = document.querySelector("#search-bar");
-// searchCity.addEventListener("submit", updateCity);
-
-// let currentLocation = document.querySelector("#current-location");
-// currentLocation.addEventListener("submit", updateCurrentLocationTemp);
-
-// function here(position) {
-//   console.log(position.coords.longitude);
-//   console.log(position.coords.latitude);
-
-//   let currentLongitude = position.coords.longitude;
-//   let currentLatitude = position.coords.latitude;
-
-//   let apiKey = "0e058f62fe2dafe733ff53545a2cf37c";
-//   let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}&units=metric`;
-
-//   axios.get(apiURL).then(showTemperature);
-// }
-
-// function showTemperature
-
-// navigator.geolocation.getCurrentPosition(here);
-
-// let now = new Date();
-
-// let current = document.querySelector("#date-time");
-
-// let days = [
-//   "Sunday",
-//   "Monday",
-//   "Tuesday",
-//   "Wednesday",
-//   "Thursday",
-//   "Friday",
-//   "Saturday"
-// ];
-// let currentDay = days[now.getDay()];
-
-// let currentHour = now.getHours();
-// let currentMinutes = now.getMinutes();
-
-// current.innerHTML = `${currentDay} | ${currentHour}:${currentMinutes}`;
-
-// function changeTemp(event) {
-//   event.preventDefault();
-//   currentTemperature.innerHTML = `20°`;
-// }
-
-// let celsius = document.querySelector(".celsius");
-// celsius.addEventListener("click", changeTemp);
-
-// function changeTempBack(event) {
-//   event.preventDefault();
-//   currentTemperature.innerHTML = `77°`;
-// }
-
-// let farenheit = document.querySelector(".farenheit");
-// farenheit.addEventListener("click", changeTempBack);
+search ("New York");
